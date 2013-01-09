ed1e0fd83bd4ae47399d85129772f97b0476f711
Morten Nilsen <morten@runsafe.no>
2013-01-09 22:25:08 +0100

    Initialized new plugin WorldEditBridge from template

---
+++ src/no/runsafe/plugin_name/Plugin.java
@@ -0,0 +1,12 @@
+package no.runsafe.worldeditbridge;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class Plugin extends RunsafePlugin
+{
+	@Override
+	protected void PluginSetup()
+	{
+		addComponent(Component.class);
+	}
+}
