efaa5f2643d1ff6682c5d290335b3cefe37259ab
Morten Nilsen <morten@runsafe.no>
2013-06-16 15:57:09 +0200

    Support for regeneration

--- src/no/runsafe/worldeditbridge/WorldEditInterface.java
+++ src/no/runsafe/worldeditbridge/WorldEditInterface.java
@@ -1,17 +1,22 @@
 package no.runsafe.worldeditbridge;
 
-import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.*;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import com.sk89q.worldedit.bukkit.selections.CuboidSelection;
+import com.sk89q.worldedit.masks.Mask;
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.regions.RegionSelector;
+import no.runsafe.framework.api.IOutput;
 import no.runsafe.framework.minecraft.RunsafeLocation;
 import no.runsafe.framework.minecraft.RunsafeServer;
 import no.runsafe.framework.minecraft.player.RunsafePlayer;
 
 public class WorldEditInterface
 {
-	public WorldEditInterface()
+	public WorldEditInterface(IOutput output)
 	{
 		worldEdit = RunsafeServer.Instance.getPlugin("WorldEdit");
+		console = output;
 	}
 
 	public void select(RunsafePlayer player, RunsafeLocation pos1, RunsafeLocation pos2)
@@ -22,5 +27,40 @@ public class WorldEditInterface
 		worldEdit.setSelection(player.getRawPlayer(), selection);
 	}
 
-	WorldEditPlugin worldEdit;
+	public boolean regenerate(RunsafePlayer runsafePlayer, RunsafeLocation pos1, RunsafeLocation pos2)
+	{
+		LocalPlayer player = worldEdit.wrapPlayer(runsafePlayer.getRawPlayer());
+		LocalSession localSession = worldEdit.getSession(runsafePlayer.getRawPlayer());
+
+		Vector low = new Vector(pos1.getBlockX(), pos1.getBlockY(), pos1.getBlockZ());
+		Vector high = new Vector(pos2.getBlockX(), pos2.getBlockY(), pos2.getBlockZ());
+
+		RegionSelector selector = localSession.getRegionSelector(player.getWorld());
+		selector.explainPrimarySelection(player, localSession, low);
+		selector.explainSecondarySelection(player, localSession, high);
+
+		Region selection;
+		try
+		{
+			selection = localSession.getSelection(player.getWorld());
+			if (selection == null)
+				return false;
+		}
+		catch (IncompleteRegionException e)
+		{
+			console.logException(e);
+			return false;
+		}
+
+		EditSession editSession = worldEdit.createEditSession(runsafePlayer.getRawPlayer());
+		Mask mask = editSession.getMask();
+		editSession.setMask(null);
+		player.getWorld().regenerate(selection, editSession);
+		editSession.setMask(mask);
+
+		return true;
+	}
+
+	private final WorldEditPlugin worldEdit;
+	private final IOutput console;
 }
