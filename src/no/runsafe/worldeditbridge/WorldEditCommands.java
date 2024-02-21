c5534eab6e0dccb96557df689bb6514a7be6fe75
Morten Nilsen <morten@runsafe.no>
2024-02-21 01:20:47 +0100

    static code analysis, resolved potential bugs

--- src/no/runsafe/worldeditbridge/WorldEditCommands.java
+++ src/no/runsafe/worldeditbridge/WorldEditCommands.java
@@ -14,7 +14,6 @@ import no.runsafe.framework.api.IServer;
 import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.internal.wrapper.ObjectUnwrapper;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 public class WorldEditCommands implements WorldEditInterface
@@ -30,8 +29,8 @@ public class WorldEditCommands implements WorldEditInterface
 	{
 		Vector pt1 = new Vector(pos1.getBlockX(), pos1.getBlockY(), pos1.getBlockZ());
 		Vector pt2 = new Vector(pos2.getBlockX(), pos2.getBlockY(), pos2.getBlockZ());
-		final CuboidSelection selection = new CuboidSelection((World) ObjectUnwrapper.convert(player.getWorld()), pt1, pt2);
-		worldEdit.setSelection((Player) ObjectUnwrapper.convert(player), selection);
+		final CuboidSelection selection = new CuboidSelection(ObjectUnwrapper.convert(player.getWorld()), pt1, pt2);
+		worldEdit.setSelection(ObjectUnwrapper.convert(player), selection);
 	}
 
 	/*
